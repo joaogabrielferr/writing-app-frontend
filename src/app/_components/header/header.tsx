d262afe98b531e1940eb08cee2fdb4bc5a24cf6b
Joao Gabriel <joaogabrielferr@gmail.com>
2025-04-03 23:22:55 -0300

    finish responsiveness

--- src/app/_components/header/header.tsx
+++ src/app/_components/header/header.tsx
@@ -1,8 +1,14 @@
 
+'use client'
+
 import Link from "next/link";
 import style from "./header.module.css";
+import { useState } from "react";
 
 export default function Header(){
+
+    const [user,setUser] = useState(null);
+
 
     return (
         <header id = "header" className = {style.header}>
